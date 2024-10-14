ce13290573d6cdd47b7c6446dd953f649be30123
DanielDuarte-Developer <daniel.duarte@cmas-systems.com>
2024-10-14 16:45:21 +0100

    Converted to mysql workbench

--- BackEndBD/storedProcedures/20241012_0458_DB_INSERT_spInsertUpdateDeleteFlight.sql
+++ BackEndBD/storedProcedures/20241012_0458_DB_INSERT_spInsertUpdateDeleteFlight.sql
@@ -1,76 +1,71 @@
-DELIMITER $$
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 -- =============================================
 -- Author:		<Daniel Duarte>
 -- Create date: <2024-10-12>
 -- =============================================
 
-CREATE PROCEDURE [dbo].[spInsertUpdateDeleteFlight]
-    @Id UNIQUEIDENTIFIER OUTPUT
-    -- Verificar Atributos
-    --@FlightCode VARCHAR(10),
-    --@Departure VARCHAR(3),
-    --@Arrival VARCHAR(3),
-    @Status NVARCHAR(MAX),
-    IN distance INT
-AS
-IF @Id IS NOT NULL
-    BEGIN
-        IF @Status = 'X'
-            BEGIN
-				UPDATE [dbo].[Flight]
-				SET 
-					[SysStatus] = @Status,
-					[SysModifyDate] = GETUTCDATE(),
-					[SysModifyUserId] = @UserId
-				WHERE [Id] = @Id
-			END
-        ELSE
-			BEGIN
-				UPDATE [dbo].[Flight]
-				SET 
-					-- Ver Atributos
-					[SysStatus] = @Status,
-					[SysModifyDate] = GETUTCDATE(),
-					[SysModifyUserId] = @UserId
-				WHERE [Id] = @Id AND [SysRowVersion] = @RowVersion
-			END
-		END
-        INSERT INTO voos (flight_code, departure, arrival, distance)
-        VALUES (flightCode, departure, arrival, distance);
-    END $$
-ELSE
-	BEGIN
-		SET @Id = NEWID()
-		INSERT INTO [dbo].[Reservation]
-		(
-			-- Ver Atributos
-			[SysStatus],
-			[SysCreateDate],
-			[SysCreateUserId],
-			[SysModifyDate],
-			[SysModifyUserId]
-		)
-		Values
-		(
-			-- Ver Atributos
-			@Status,
-			GETUTCDATE(),
-			@UserId,
-			GETUTCDATE(),
-			@UserId
-		)
-	END
+DELIMITER $$
 
-IF ( @@ROWCOUNT > 0 )
-	BEGIN
-		SELECT @Id
-	END
-ELSE
-	BEGIN
-		SELECT @Id = NULL
-	END
+CREATE PROCEDURE spInsertUpdateDeleteFlight(
+    INOUT p_Id CHAR(36), -- Equivalente ao UNIQUEIDENTIFIER no SQL Server
+    IN p_Status NVARCHAR(255), -- Adaptei o tamanho, ajuste conforme necessário
+    IN p_UserId CHAR(36), -- Presume que o UserId seja um UUID
+    IN p_RowVersion CHAR(36), -- Equivalente ao SysRowVersion (ajustar conforme o uso)
+    IN p_FlightCode VARCHAR(10), -- Defini os parâmetros que estavam comentados
+    IN p_Departure VARCHAR(3),
+    IN p_Arrival VARCHAR(3),
+    IN p_Distance INT
+)
+BEGIN
+    IF p_Id IS NOT NULL THEN
+        IF p_Status = 'X' THEN
+            UPDATE Flight
+            SET 
+                SysStatus = p_Status,
+                SysModifyDate = UTC_TIMESTAMP(),
+                SysModifyUserId = p_UserId
+            WHERE Id = p_Id;
+        ELSE
+            UPDATE Flight
+            SET 
+                -- Verifique e adicione os campos necessários
+                SysStatus = p_Status,
+                SysModifyDate = UTC_TIMESTAMP(),
+                SysModifyUserId = p_UserId
+            WHERE Id = p_Id AND SysRowVersion = p_RowVersion;
+        END IF;
+        
+        -- Inserindo na tabela 'voos'
+        INSERT INTO voos (flight_code, departure, arrival, distance)
+        VALUES (p_FlightCode, p_Departure, p_Arrival, p_Distance);
+    ELSE
+        SET p_Id = UUID(); -- Gerando novo UUID
+        INSERT INTO Reservation
+        (
+            -- Verifique os campos necessários
+            SysStatus,
+            SysCreateDate,
+            SysCreateUserId,
+            SysModifyDate,
+            SysModifyUserId
+        )
+        VALUES
+        (
+            -- Verifique e adicione os valores
+            p_Status,
+            UTC_TIMESTAMP(),
+            p_UserId,
+            UTC_TIMESTAMP(),
+            p_UserId
+        );
+    END IF;
+
+    -- Verificar se a linha foi modificada
+    IF ROW_COUNT() > 0 THEN
+        SELECT p_Id;
+    ELSE
+        SET p_Id = NULL;
+        SELECT p_Id;
+    END IF;
+END $$
+
 DELIMITER ;
--- BackEndBD/storedProcedures/20241012_0507_DB_SELECT_spGetFlights.sql
+++ BackEndBD/storedProcedures/20241012_0507_DB_SELECT_spGetFlights.sql
@@ -1,55 +1,54 @@
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 -- =============================================
 -- Author:		<Daniel Duarte>
 -- Create date: <2024-10-12>
 -- =============================================
-CREATE PROCEDURE [dbo].[spGetFlights]
-	@Id UNIQUEIDENTIFIER = NULL,
-    -- Verificar Atributos	
-    --@FlightCode VARCHAR(10) = NULL,
-    --@Departure VARCHAR(3) = NULL,
-    --@Arrival VARCHAR(3) = NULL,
-	@UserId VARCHAR(255) = NULL,
-	@Status VARCHAR(255) = NULL,
-	@SortField NVARCHAR(50) = 'Id',
-	@SortOrder NVARCHAR(4) = 'ASC',
-	@Skip INT = 0,
-	@Take INT = 1000000
-AS
-SELECT
-	*
+DELIMITER $$
 
-FROM [dbo].[Flight]
-WHERE (@Id IS NULL OR Id = @Id)
--- Verificar os atributos de ser null
-	--AND (@RoomId IS NULL OR RoomId = @RoomId)
-	 --AND (
-            --(@startDateTime IS NULL AND @endDateTime IS NULL)
-            --OR (@startDateTime IS NOT NULL AND @endDateTime IS NOT NULL AND startDateTime BETWEEN @startDateTime AND @endDateTime)
-            --OR (@startDateTime IS NOT NULL AND @endDateTime IS NULL AND startDateTime >= @startDateTime)
-            --OR (@startDateTime IS NULL AND @endDateTime IS NOT NULL AND startDateTime <= @endDateTime)
-        --)
-	AND (@UserId IS NULL OR SysCreateUserId = @UserId)
-	AND (@Status IS NULL OR SysStatus = @Status)
-	AND (SysStatus != 'X')
-ORDER BY 
-		CASE WHEN @SortOrder = 'ASC' AND @SortField = 'Id' THEN [Id] END ASC,
-		CASE WHEN @SortOrder = 'DESC' AND @SortField = 'Id' THEN [Id] END DESC,
-		
-
-		CASE WHEN @SortOrder = 'ASC' AND @SortField = 'SysStatus' THEN [SysStatus] END ASC,
-		CASE WHEN @SortOrder = 'DESC' AND @SortField = 'SysStatus' THEN [SysStatus] END DESC,
-		CASE WHEN @SortOrder = 'ASC' AND @SortField = 'SysCreateDate' THEN [SysCreateDate] END ASC,
-		CASE WHEN @SortOrder = 'DESC' AND @SortField = 'SysCreateDate' THEN [SysCreateDate] END DESC,
-		CASE WHEN @SortOrder = 'ASC' AND @SortField = 'SysCreateUserId' THEN [SysCreateUserId] END ASC,
-		CASE WHEN @SortOrder = 'DESC' AND @SortField = 'SysCreateUserId' THEN [SysCreateUserId] END DESC,
-		CASE WHEN @SortOrder = 'ASC' AND @SortField = 'SysModifyDate' THEN [SysModifyDate] END ASC,
-		CASE WHEN @SortOrder = 'DESC' AND @SortField = 'SysModifyDate' THEN [SysModifyDate] END DESC,
-		CASE WHEN @SortOrder = 'ASC' AND @SortField = 'SysModifyUserId' THEN [SysModifyUserId] END ASC,
-		CASE WHEN @SortOrder = 'DESC' AND @SortField = 'SysModifyUserId' THEN [SysModifyUserId] END DESC
-	OFFSET @Skip ROWS
-	FETCH NEXT @Take ROWS ONLY
-RETURN 0
+CREATE PROCEDURE spGetFlights(
+    IN p_Id CHAR(36) DEFAULT NULL,
+    -- Ajuste para os parâmetros que estavam comentados
+    -- IN p_FlightCode VARCHAR(10) DEFAULT NULL,
+    -- IN p_Departure VARCHAR(3) DEFAULT NULL,
+    -- IN p_Arrival VARCHAR(3) DEFAULT NULL,
+    IN p_UserId VARCHAR(255) DEFAULT NULL,
+    IN p_Status VARCHAR(255) DEFAULT NULL,
+    IN p_SortField VARCHAR(50) DEFAULT 'Id',
+    IN p_SortOrder VARCHAR(4) DEFAULT 'ASC',
+    IN p_Skip INT DEFAULT 0,
+    IN p_Take INT DEFAULT 1000000
+)
+BEGIN
+    -- Query principal
+    SELECT
+        *
+    FROM Flight
+    WHERE (p_Id IS NULL OR Id = p_Id)
+    -- Verificar os atributos comentados (adicionar condições se necessário)
+        -- AND (p_RoomId IS NULL OR RoomId = p_RoomId)
+        -- AND (
+                -- (p_startDateTime IS NULL AND p_endDateTime IS NULL)
+                -- OR (p_startDateTime IS NOT NULL AND p_endDateTime IS NOT NULL AND startDateTime BETWEEN p_startDateTime AND p_endDateTime)
+                -- OR (p_startDateTime IS NOT NULL AND p_endDateTime IS NULL AND startDateTime >= p_startDateTime)
+                -- OR (p_startDateTime IS NULL AND p_endDateTime IS NOT NULL AND startDateTime <= p_endDateTime)
+            -- )
+        AND (p_UserId IS NULL OR SysCreateUserId = p_UserId)
+        AND (p_Status IS NULL OR SysStatus = p_Status)
+        AND (SysStatus != 'X')
+    -- Ordenação com CASE
+    ORDER BY 
+        CASE WHEN p_SortOrder = 'ASC' AND p_SortField = 'Id' THEN Id END ASC,
+        CASE WHEN p_SortOrder = 'DESC' AND p_SortField = 'Id' THEN Id END DESC,
+        CASE WHEN p_SortOrder = 'ASC' AND p_SortField = 'SysStatus' THEN SysStatus END ASC,
+        CASE WHEN p_SortOrder = 'DESC' AND p_SortField = 'SysStatus' THEN SysStatus END DESC,
+        CASE WHEN p_SortOrder = 'ASC' AND p_SortField = 'SysCreateDate' THEN SysCreateDateEND ASC,
+        CASE WHEN p_SortOrder = 'DESC' AND p_SortField = 'SysCreateDate' THEN SysCreateDate END DESC,
+        CASE WHEN p_SortOrder = 'ASC' AND p_SortField = 'SysCreateUserId' THEN SysCreateUserId END ASC,
+        CASE WHEN p_SortOrder = 'DESC' AND p_SortField = 'SysCreateUserId' THEN SysCreateUserId END DESC,
+        CASE WHEN p_SortOrder = 'ASC' AND p_SortField = 'SysModifyDate' THEN SysModifyDateEND ASC,
+        CASE WHEN p_SortOrder = 'DESC' AND p_SortField = 'SysModifyDate' THEN SysModifyDateEND DESC,
+        CASE WHEN p_SortOrder = 'ASC' AND p_SortField = 'SysModifyUserId' THEN SysModifyUserId END ASC,
+        CASE WHEN p_SortOrder = 'DESC' AND p_SortField = 'SysModifyUserId' THEN SysModifyUserId END DESC
+    -- Paginação com LIMIT e OFFSET
+    LIMIT p_Take OFFSET p_Skip;
+END $$
+DELIMITER ;
