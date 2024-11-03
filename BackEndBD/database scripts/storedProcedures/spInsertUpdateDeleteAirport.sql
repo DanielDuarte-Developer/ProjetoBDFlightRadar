cff9209e48d0443552e26658abc3abe77c85f155
unknown <tiagomtomas11b@gmail.com>
2024-11-03 10:08:19 +0000

    Fixed scripts syntax -- need to be tested -- gets still have syntax problems

--- BackEndBD/database scripts/storedProcedures/spInsertUpdateDeleteAirport.sql
+++ BackEndBD/database scripts/storedProcedures/spInsertUpdateDeleteAirport.sql
@@ -1,52 +1,58 @@
 -- =============================================
 -- Author:		<Daniel Duarte>
--- Create date: <2024-10-28>
+-- Create date: <2024-10-12>
 -- =============================================
-
+use flight_radar;
 DELIMITER $$
 
-CREATE PROCEDURE spInsertUpdateDeleteAirportAirplaneFlight(
+CREATE PROCEDURE spInsertUpdateDeleteAirport(
     -- DB atributes
-    IN p_IdAirport CHAR(36),
-    IN p_IdFlight CHAR(36),
-    IN p_IdAirplane CHAR(36),
-    IN p_Departure DateTime,
-    IN p_Arrival DateTime,
+    INOUT p_Id CHAR(36),
+    IN p_IdCountry CHAR(36),
+    IN p_AirportName NVARCHAR(100),
+    IN p_AirportCode CHAR(10),
+    IN p_LocationName NVARCHAR(100),
+    IN p_LocationLatitude INT,
+    IN p_LocationLongitude INT,
     -- Control atributes
     IN p_Status NVARCHAR(255), 
     IN p_UserId CHAR(36),
-    IN p_RowVersion CHAR(36),
+    IN p_RowVersion CHAR(36)
 )
 BEGIN
     IF p_Id IS NOT NULL THEN
         IF p_Status = 'X' THEN
-            UPDATE airport_airplane_flight
+            UPDATE airport
             SET 
                 sys_status = p_Status,
                 sys_modify_date = UTC_TIMESTAMP(),
                 sys_modify_user_id = p_UserId
             WHERE Id = p_Id;
         ELSE
-            UPDATE airport_airplane_flight
+            UPDATE airport
             SET 
-                id_airport = p_IdAirport,
-                id_flight = p_IdFlight,
-                id_airplane = p_IdAirplane,
-                departure = p_Departure,
-                arrival = p_Arrival,
+                id_country = p_IdCountry,
+                airport_name = p_AirportName,
+                airport_code = p_AirportCode,
+                location_name = p_LocationName,
+                location_latitude = p_LocationLatitude,
+                location_longitude = p_LocationLongitude,
                 sys_status = p_Status,
                 sys_modify_date = UTC_TIMESTAMP(),
                 sys_modify_user_id = p_UserId
             WHERE Id = p_Id AND row_version = p_RowVersion;
         END IF;
     ELSE
-        INSERT INTO airport_airplane_flight
+        SET p_Id = UUID();
+        INSERT INTO airport
         (
-            id_airport,
-            id_flight,
-            id_airplane,
-            departure,
-            arrival,
+            id_plane,
+            id_country,
+            airport_name,
+            airport_code,
+            location_name,
+            location_latitude,
+            location_longitude,
             sys_status,
             sys_create_date,
             sys_create_user_id,
@@ -55,11 +61,13 @@ BEGIN
         )
         VALUES
         (
-            p_IdAirport,
-            p_IdFlight,
-            p_IdAirplane,
-            p_Departure,
-            p_Arrival,
+            p_Id,
+            p_IdCountry,
+            p_AirportName,
+            p_AirportCode,
+            p_LocationName,
+            p_LocationLatitude,
+            p_LocationLongitude,
             p_Status,
             UTC_TIMESTAMP(),
             p_UserId,
