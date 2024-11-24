a5542e7116688ae00189687060fa376f95dfb425
unknown <tiagomtomas11b@gmail.com>
2024-11-24 15:01:32 +0000

    Added map values for planes

--- BackEndBD/DBScripts/storedProcedures/AuxiliaryBehaviours.sql
+++ BackEndBD/DBScripts/storedProcedures/AuxiliaryBehaviours.sql
@@ -2,95 +2,59 @@ use flight_radar;
 
 DELIMITER $$
 create function randomFlightObservation()
-returns varchar
+returns varchar(100)
 begin
 
 declare observationsNumber int; 
 declare observationId varchar(100);
 
-select count(*) into observationsNumber from observation;
-select IdObservation into observationId from observation where IdObservation = rand(@observationsNumber);
+select IdObservation into observationId from observation order by rand() limit 1;
 
 return observationId;
 end $$
-create function getDepartureAirport(flightId varchar(100))
-returns varchar(100)
+
+create procedure getDeparture(in flightId varchar(100), out airportId varchar(100), out timeMarker timestamp)
 begin
-    SET @airportId :=(select IdAirport from airport_flight where IdFlight = flightId order by TimeMarker asc limit 1);
-return @airportId;    
+    select IdAirport, TimeMarker into airportId, timeMarker from airport_flight where IdFlight = flightId order by TimeMarker asc limit 1;
 end $$
 
-create function getArrivalAirport(flightId varchar(100))
-returns varchar(100)
+create procedure getArrival(flightId varchar(100), out airportId varchar(100), out timeMarker timestamp)
 begin
-    SET @airportId :=(select IdAirport from airport_flight where IdFlight = flightId order by TimeMarker desc limit 1);
-return @airportId;    
+    select IdAirport, TimeMarker into airportId, timeMarker from airport_flight where IdFlight = flightId order by TimeMarker desc limit 1;
 end $$
 
-create function getStopOversAirport(flightId varchar(100))
-returns varchar(100)
+create procedure getStopOvers(flightId varchar(100), out airportId varchar(100), out timeMarker timestamp)
 begin
-    SET @airportId :=(select IdAirport from airport_flight where IdFlight = flightId and TimeMarker > (select TimeMarker from airport_flight order by TimeMarker asc limit 1) and TimeMarker < (select TimeMarker from airport_flight order by TimeMarker desc limit 1));
-return @airportId;    
+    select IdAirport, TimeMarker into airportId, timeMarker from airport_flight where IdFlight = flightId and TimeMarker > (select TimeMarker from airport_flight order by TimeMarker asc limit 1) and TimeMarker < (select TimeMarker from airport_flight order by TimeMarker desc limit 1);
 end $$
 
-create function getDepartureTimeMarker(flightId varchar(100))
-returns timestamp
+create procedure getMapPlaneValues(out flightId varchar(100), out startLat int, out startLong int, out endLat int, out endLong int)
 begin
-    SET @timeMarker :=(select TimeMarker from airport_flight where TimeMarker = timeMarker order by TimeMarker asc limit 1);
-return @timeMarker;    
-end $$
-
-create function getArrivalTimeMarker(flightId varchar(100))
-returns timestamp
-begin
-    SET @timeMarker :=(select TimeMarker from airport_flight where TimeMarker = timeMarker order by TimeMarker desc limit 1);
-return @timeMarker;    
-end $$
-
-create function getStopOversTimeMarker(flightId varchar(100))
-returns timestamp
-begin
-    SET @timeMarkers :=(select TimeMarker from airport_flight where TimeMarker > (select TimeMarker from airport_flight order by TimeMarker asc limit 1) and TimeMarker < (select TimeMarker from airport_flight order by TimeMarker desc limit 1);
-return @timeMarkers;    
-end $$
-
-create procedure getDeparture(flightId varchar(100))
-begin
-    select IdAirport, TimeMarker from airport_flight where IdFlight = flightId order by TimeMarker asc limit 1;
-end $$
-
-create procedure getArrival(flightId varchar(100))
-begin
-    select IdAirport, TimeMarker from airport_flight where IdFlight = flightId order by TimeMarker desc limit 1;
-end $$
-
-create procedure getStopOvers(flightId varchar(100))
-begin
-    select IdAirport, TimeMarker from airport_flight where IdFlight = flightId and TimeMarker > (select TimeMarker from airport_flight order by TimeMarker asc limit 1) and TimeMarker < (select TimeMarker from airport_flight order by TimeMarker desc limit 1);
-end $$
-
-create procedure getMapPlaneValues()
-begin
-    declare flightId varchar(100)
     declare done int default false;
-    declare continue handler for not found set done = true;
+	declare airportId varchar(100);
+    declare timeMarker timestamp;
+    set flightId = '';
+    set startLat = 0; 
+    set startLong = 0; 
+    set endLat = 0; 
+    set endLong = 0;
+    declare cursor cursorFlightIds for select IdFlight from flight;
+	declare continue handler for not found set done = true;
 
-    declare cursor flightIds for 
+    open cursorFlightIds;
     
-    select IdFlight from flight;
-    open flightIds;
     read_loop : loop
-        fetch flightIds into flightId;
+        fetch cursorFlightIds into flightId;
         if done then leave read_loop;
         end if;
-        call getDeparture(flightId);
-        declare departure
-        call getArrival(flightId);
-        call getStopOvers(flightId);
+        
+        call getDeparture(flightId, airportId, timeMarker);
+        select LocationLatitude, LocationLongitude into startLat, startLong from airport where IdAirport = airportId;
+       
+       call getArrival(flightId, airportId, timeMarker);
+        select LocationLatitude, LocationLongitude into endLat, endLong from airport where IdAirport = airportId;
     end loop;
-    create view getMapPlaneValues as select IdFlight, 
-
+    close cursorFlightIds;
 end $$
 
 DELIMITER ;
